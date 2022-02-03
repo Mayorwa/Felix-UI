25d557525386e2d2b9f352d36ca662e7320f11d9
Mayorwa <mayowa.ogunwole@mypatricia.co>
2022-02-03 17:49:02 +0100

    initial files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="layoutComponent">
+    <router-view />
+  </component>
+</template>
+
+<script>
+import defaultLayout from '@/components/layouts/default'
+export default {
+  name: 'App',
+  computed: {
+    layoutComponent() {
+      let currentLayout = ''
+      const routeLayout = this.$route.meta.layout?.toLowerCase()
+      switch (routeLayout) {
+        case 'default':
+          currentLayout = defaultLayout
+          break
+      }
+      return currentLayout
+    },
+  },
+}
+</script>
+<style lang="scss">
+@import 'assets/sass/main.scss';
+</style>
